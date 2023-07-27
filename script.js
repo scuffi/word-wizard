f1bd4c46f2f8b7d21b49838b9dd33480c67517ff
Scuffi <grawlixpvp@gmail.com>
2023-07-27 20:44:30 +0100

    Added some flexsearch logic

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
 // Your code using FlexSearch goes here
-const index = new FlexSearch.Index();
+const searchIndex = new FlexSearch.Index();
 
 async function init() {
-    console.log("Loaded Word wizard");
+    buildSearchIndex();
 }
+
+function search(word) {
+    const matches = searchWords(word);
+    highlightWords(matches);
+}
+
+function highlightWords(matches) {
+    if (matches) {
+        matches.forEach((word, index) => {
+            alert(word)
+            document.body.innerHTML = document.body.innerHTML.replace(
+              word,
+              (match) => `<span class="highlight">${match}</span>`
+            );
+        })
+    }
+  }
+
+function buildSearchIndex() {
+    const words = document.body.innerText.split(/\s+/);
+    words.forEach((word, index) => {
+        alert(word)
+        searchIndex.add(index, word);
+    });
+  }
+
+  function searchWords(word) {
+    const results = searchIndex.search(word);
+    return results.length ? results : [];
+  }
+
+  function removeAllHighlights() {
+    const highlights = document.querySelectorAll(".highlight");
+    highlights.forEach((highlight) => {
+      highlight.outerHTML = highlight.innerHTML;
+    });
+  }
+
+  function scrollToMatch(index) {
+    const highlight = document.querySelectorAll(".highlight")[index];
+    if (highlight) {
+      highlight.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
 init();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById("searchInput");
+    const searchBtn = document.getElementById("searchBtn");
+    const countLabel = document.getElementById("countLabel");
+  
+    searchBtn.addEventListener("click", () => {
+      const word = searchInput.value;
+      search(word);
+    });
+  });
+  
